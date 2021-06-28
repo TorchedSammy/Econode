1375f3703639f43c96b220982620e3e3619e9965
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-06-28 14:00:20 -0400

    fix: also return error from rows

--- econetwork/account.go
+++ econetwork/account.go
@@ -27,8 +27,8 @@ type Node struct {
 }
 
 func (e *Econetwork) getAccount(username string) (*Account, error) {
-	_, err := e.db.Query("SELECT * FROM users WHERE username = ?;", username)
-	return nil, err
+	rows, err := e.db.Query("SELECT * FROM users WHERE username = ?;", username)
+	return nil, err || rows.Err()
 }
 
 func (e *Econetwork) register(r RegisterPayload) error {
